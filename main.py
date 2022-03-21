c9109a113033d48c15f6760fd50b712039a532e1
luizokada <luizfernandookada@gmail.com>
2022-03-21 10:13:24 -0300

    Funcao de leitura do arquivo e paste de testes

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 
+
 import sys
+
+
+def getVertices(caminho):
+    arq = open(caminho)
+    descricao = []
+    vertices = []
+    for line in arq:
+        descricao.append(line)
+    index = descricao.index('NODE_COORD_SECTION\n')
+    construtor = descricao[index + 1: len(descricao)-1]
+    for i in range(len(construtor)):
+        vertices.append(construtor[i].split(' ', 3))
+    return vertices
 
 
 def main():
     x = 33522
-    print(x)
+    caminho = sys.argv[1]
+    construtor = getVertices(caminho)
+    print(construtor)
 
 
 main()
